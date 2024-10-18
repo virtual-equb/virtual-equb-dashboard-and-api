b1116db39323dbe72b327339d20432202c9519c3
virtualequb <equbvirtual@gmail.com>
2024-10-18 04:30:26 -0700

    Create views for city and subcity management

---
+++ app/Http/Controllers/SubCityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Repositories\City\ICityRepository;
+use App\Repositories\SubCity\ISubCityRepository;
+use Exception;
+
+class SubCityController extends Controller
+{
+    private $cityRepository;
+    private $subCityRepository;
+
+    public function __construct(ISubCityRepository $subCityRepository)
+    {
+        $this->subCityRepository = $subCityRepository;
+    }
+
+    /**
+     * Get sub-cities by city ID.
+     *
+     * @param  int  $cityId
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getSubCitiesByCityId($cityId)
+    {
+        try {
+            $subCities = $this->subCityRepository->getSubCityByCityId($cityId);
+            return response()->json($subCities);
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Failed to retrieve sub-cities.'], 500);
+        }
+    }
+}
