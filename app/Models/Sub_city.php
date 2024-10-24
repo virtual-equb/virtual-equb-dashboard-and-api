7ae1247dcfab061ae60f14b830031be8cbf6be9b
virtualequb <equbvirtual@gmail.com>
2024-10-24 06:11:25 -0700

    edit subcity

--- app/Models/Sub_city.php
+++ app/Models/Sub_city.php
@@ -8,22 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Sub_city extends Model
 {
     use HasFactory;
-
-
-    protected $fillable = [
-        'created_by',
-        'city_id',
-        'name',
-        'active',
-        'remark'
-    ];
-
-    public function city() {
-        return $this->belongsTo(Cities::class, 'city_id');
-    }
-
-    public function subCreater()
-    {
-        return $this->belongsTo(User::class, 'created_by');
-    }
 }
