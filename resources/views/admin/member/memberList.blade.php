c68a71c9b7dca53b0e1176cbf028bd7de3541bbd
virtualequb <equbvirtual@gmail.com>
2024-10-30 09:02:06 -0700

    edit

--- resources/views/admin/member/memberList.blade.php
+++ resources/views/admin/member/memberList.blade.php
@@ -1,10 +1,3 @@
-{{-- @if (Auth::user()->role == 'admin' ||
-        Auth::user()->role == 'general_manager' ||
-        Auth::user()->role == 'operation_manager' ||
-        Auth::user()->role == 'finance' ||
-        Auth::user()->role == 'customer_service' ||
-        Auth::user()->role == 'assistant' ||
-        Auth::user()->role == 'it') --}}
     @extends('layouts.app')
     @section('styles')
         <style type="text/css">
