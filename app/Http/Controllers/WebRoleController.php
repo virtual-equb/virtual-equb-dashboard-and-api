8d7a13656a19844bb63296405e6fb2242f3332c1
virtualequb <equbvirtual@gmail.com>
2024-10-30 04:10:03 -0700

    Update WebRoleController.php

--- app/Http/Controllers/WebRoleController.php
+++ app/Http/Controllers/WebRoleController.php
@@ -27,11 +27,11 @@ class WebRoleController extends Controller
         $this->userRepository = $userRepository;
         $this->title = "Virtual Equb - User";
 
-        // Guard Permission
-        $this->middleware('permission:edit role', ['only' => ['update', 'edit', 'updatePermissionToRole']]);
-        $this->middleware('permission:delete role', ['only' => ['destroy']]);
-        $this->middleware('permission:view role', ['only' => ['index', 'show']]);
-        $this->middleware('permission:create role', ['only' => ['store', 'create', 'addPermissionToRole']]);
+        // // Guard Permission
+        // $this->middleware('permission:edit role', ['only' => ['update', 'edit', 'updatePermissionToRole']]);
+        // $this->middleware('permission:delete role', ['only' => ['destroy']]);
+        // $this->middleware('permission:view role', ['only' => ['index', 'show']]);
+        // $this->middleware('permission:create role', ['only' => ['store', 'create', 'addPermissionToRole']]);
     }
     public function index()
     {
