31c8c3916ac6401833a0d731c0567918defc411c
virtualequb <equbvirtual@gmail.com>
2024-10-31 01:22:18 -0700

    edit

--- storage/framework/views/de74a39b0010c0d4ca219b3ff9fd991ccbcffa7b.php
+++ storage/framework/views/de74a39b0010c0d4ca219b3ff9fd991ccbcffa7b.php
@@ -275,6 +275,7 @@
                         <?php echo method_field('PUT'); ?>
                         <p class="text-center">Are you sure you want to update status?</p>
                     </div>
+                    <h1></h1>
                     <div class="modal-footer">
                         <button type="submit" class="btn btn-sm btn-danger">update</button>
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
