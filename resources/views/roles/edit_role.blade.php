f5885d4cc4f05a28aba54903d02d2cab519d3dcb
virtualequb <equbvirtual@gmail.com>
2024-11-05 21:19:47 -0800

    revised role

--- resources/views/roles/edit_role.blade.php
+++ resources/views/roles/edit_role.blade.php
@@ -51,7 +51,7 @@
                                     </label>
                                     <input class="form-control" type="text" placeholder="{{ get_label('please_enter_role_name', 'Please enter role name') }}" id="name" name="name" value="{{ $role->name }}" required>
                                     @error('name')
-                                    <p class="text-danger text-xs mt-1">{{ $message }}</p>
+                                    <p class="text-danger text-xs mt-1">{{ $messages }}</p>
                                     @enderror
                                 </div>
                               
