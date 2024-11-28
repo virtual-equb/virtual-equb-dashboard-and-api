2c5ace785c4a5d7fde7366307d417e1c8571a3ec
virtualequb <equbvirtual@gmail.com>
2024-11-28 03:26:37 -0800

    edit seasonal draw

--- resources/views/admin/equbType/addEqubType.blade.php
+++ resources/views/admin/equbType/addEqubType.blade.php
@@ -6,7 +6,7 @@
             <form role="form" method="post" class="form-horizontal" action="{{ route('registerEqubType') }}" enctype="multipart/form-data" id="addEqubType">
                 {{ csrf_field() }}
                 <div class="modal-header">
-                    <h4 class="modal-title">Add Equb Type</h4>
+                    <h4 class="modal-title">Add Equb Type s</h4>
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
