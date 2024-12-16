eee435ac21675b7c0e73de195db34b5c968eae8a
virtualequb <equbvirtual@gmail.com>
2024-12-16 10:17:08 +0300

    rollback

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -826,7 +826,7 @@ class PaymentController extends Controller
                     $amount = $at;
                 }
             }
-            $memberData = Member::where('id', 42)->first();
+            $memberData = Member::where('phone', $user->phone_number)->first();
 
             $paymentData = [
                 'member_id' => $memberData->id,
