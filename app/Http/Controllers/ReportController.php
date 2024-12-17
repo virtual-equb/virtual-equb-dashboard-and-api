cb7474b31221232bbe1104d1e92d76ec56d88da3
virtualequb <equbvirtual@gmail.com>
2024-12-17 00:37:04 -0800

    edit equb collector

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -881,9 +881,9 @@ class ReportController extends Controller
          //   if ($userData && ($userData['role'] == "admin" || $userData['role'] == "general_manager" || $userData['role'] == "operation_manager" || $userData['role'] == "assistant" || $userData['role'] == "finance")) {
                 $data['title'] = "Virtual Equb - Reserved Lottery Dates Report";
                 $data['equbTypes'] = $this->equbTypeRepository->getActive();
-                return response()->json($data);
+             //   return response()->json($data);
 
-              //  return view('admin/report/reservedLotteryDates/lotterys', $data);
+               return view('admin/report/reservedLotteryDates/lotterys', $data);
           
         } catch (Exception $ex) {
             $msg = "Unknown Error Occurred, Please try again!";
