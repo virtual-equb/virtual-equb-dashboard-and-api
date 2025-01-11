996845242e8d110e6cc92199e60e0a202138b26e
virtualequb <equbvirtual@gmail.com>
2025-01-11 12:00:57 +0300

    collectedBy

--- app/Repositories/Payment/PaymentRepository.php
+++ app/Repositories/Payment/PaymentRepository.php
@@ -172,6 +172,7 @@ class PaymentRepository implements IPaymentRepository
         ->whereDate('payments.created_at', '<=', $dateTo)
         ->whereRaw('LOWER(payments.status) = ?', ['paid']) // Case-insensitive check
         ->groupBy('payments.id')
+        ->where('payments.status', 'paid')
         ->offset($offset)
         ->limit($this->limit);
     
