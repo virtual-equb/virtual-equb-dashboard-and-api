d2681f002166a87a1064104b05459011385ea3c2
virtualequb <equbvirtual@gmail.com>
2025-02-13 11:08:38 +0300

    api_guard_middleware

--- app/Http/Controllers/Api/CbeMiniAppController.php
+++ app/Http/Controllers/Api/CbeMiniAppController.php
@@ -47,9 +47,10 @@ class CbeMiniAppController extends Controller
     public function validateToken(Request $request)
     {
         try {
-            $token = $request->header('Authorization');
+            // $token = $request->header('Authorization');
             // $token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZSI6IjI1MTkxODA5NDQ1NSIsImV4cCI6MTczODYzMjEzMH0.cN95szHJNoJwp8tdtpDOk29vPmQeVoYP8dbKFBFy4_M";
             // $token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZSI6IjI1MTkxODA5NDQ1NSIsImV4cCI6MTczODI3OTU0NH0.8NrfTbeErIXyin-PH0Vgvnkq4-q2TeVvQz4P3FtBqZU";
+            $token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZSI6IjI1MTkxODA5NDQ1NSIsImV4cCI6MTc0MzA0MjQxNn0.d3keyZuG0FCQpRVolicjFIgbiSqmSRywJGf4spseeeA";
             // dd($token);
             if (!$token) {
                 return response()->json([
