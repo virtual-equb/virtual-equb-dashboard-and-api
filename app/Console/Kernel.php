0f38fb985f92d2d49f8970deaae73469a8a282c9
Mikiyas Leul <mikiyasleul@gmail.com>
2025-02-28 15:56:42 +0300

    job scheduled file upated - to work with cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,36 +7,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')->hourly();
-        // $schedule->command('check:enddate')->dailyAt('9:00');
-        // $schedule->command('check:lotterydate')->dailyAt('9:00');
-        // $schedule->command('equb:draw')->dailyAt('9:00');
-        // $schedule->command('check:unpaidPayments')->dailyAt('9:00');
-        // $schedule->command('send:reminder')->dailyAt('9:00');
-        // $schedule->command('check:equbtype')->dailyAt('9:00');
-
         // Updated Schedules
-        $schedule->command('equb:notify-due-starts')->dailyAt('00:00');
-        $schedule->command('equb:notify-due-ends')->dailyAt('00:00');
-        $schedule->command('equb:sendnotifications')->dailyAt('00:00');
-        $schedule->command('equb:autoDrawLottery')->dailyAt('00:00'); // Run every day at midnight
-        $schedule->command('equb:draw-winners')->dailyAt('00:00');
-        $schedule->command('remove:removeOlderPendingTelebirrPayments')->everyThreeHours($minutes = 0);
+        $schedule->command('check:enddate')->everyEightHours();
+        $schedule->command('send:reminders')->everyNineHours()->withoutOverlapping();
+        $schedule->command('equb:draw')->everyFifteenHours();
+        $schedule->command('check:lotterydate')->everyNineHours()->withoutOverlapping();
+        $schedule->command('check:unpaidPayments')->everyTenHours();
+        $schedule->command('remove:removeOlderPendingTelebirrPayments')->everyThreeHours();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
