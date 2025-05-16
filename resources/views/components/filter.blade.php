a000e7f5d8ed93032ba6bf06ef3bafac731b2c56
Daniel <kassawdaniel7@gmail.com>
2025-05-16 15:20:58 +0300

    fix model issue

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -1,6 +1,6 @@
 <div class="p-4 mb-3" style="background: #eaf0ec; border-radius: 20px;">
     <form id="filterForm" method="GET" action="{{ $action ?? '' }}">
-        <div class="d-flex justify-content-between align-items-center mb-3">
+        <div class="mb-3 d-flex justify-content-between align-items-center">
             <div class="d-flex align-items-center">
                 <span class="mr-2" style="font-size: 1.5rem; color: #2e343b;">
                     <!-- Filter Icon SVG -->
@@ -8,11 +8,11 @@
                         <path d="M3 5a1 1 0 0 1 1-1h16a1 1 0 0 1 .8 1.6l-5.6 7.47V19a1 1 0 0 1-1.45.89l-4-2A1 1 0 0 1 9 17v-4.93L3.2 6.6A1 1 0 0 1 3 5zm2.618 1l5.382 7.18V17.38l2 1V13.18L18.382 6z"/>
                     </svg>
                 </span>
-                <span class="h5 font-weight-bold mb-0" style="color: #2d3e50;">Filters</span>
+                <span class="mb-0 h5 font-weight-bold" style="color: #2d3e50;">Filters</span>
             </div>
         </div>
-        <div class="form-row align-items-end m-0">
-            <div class="float-right  col-6" id="member_table_filter">
+        <div class="m-0 form-row align-items-end">
+            <div class="float-right col-6" id="member_table_filter">
                 <select class="form-control" id="equbSearchText" name="equb_type_id"
                     placeholder="Equb Type">
                     <option value="">All Equb Type</option>
